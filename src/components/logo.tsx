e653ff0965cd5959561307936319d670190b4b47
Magloire Kouadio <magloire078@gmail.com>
2025-09-05 16:49:53 +0000

    ok

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,24 @@
-import { School } from 'lucide-react';
 import Link from 'next/link';
 
 export function Logo() {
   return (
     <Link href="/dashboard" className="flex items-center gap-2 text-primary font-semibold">
-      <School className="h-6 w-6" />
+        <svg
+            className="h-7 w-7"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            >
+            <path d="M2 6s1.5-2 5-2 5 2 5 2v14s-1.5-1-5-1-5 1-5 1V6z" />
+            <path d="M12 6s1.5-2 5-2 5 2 5 2v14s-1.5-1-5-1-5 1-5 1V6z" />
+            <path d="M8 12h8" />
+            <path d="M8 16h8" />
+            <path d="M12 2v2" />
+        </svg>
       <h1 className="text-lg font-bold font-headline">GèreEcole</h1>
     </Link>
   );
