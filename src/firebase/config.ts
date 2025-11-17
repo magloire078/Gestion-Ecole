4da5ee421de5875db12d6e9cad3f346973f51969
Magloire Kouadio <magloire078@gmail.com>
2025-11-17 20:32:33 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-  apiKey: "AIzaSyCVGFgK7-r2p302pMh4mP4e_U_l_3_d8oU",
-  authDomain: "greecole.firebaseapp.com",
-  projectId: "greecole",
-  storageBucket: "greecole.appspot.com",
-  messagingSenderId: "97019754371",
-  appId: "1:97019754371:web:4822d9c017bf4be808e8b6"
+  "projectId": "greecole",
+  "appId": "1:97019754371:web:ca4fc0c74ec26db408e8b6",
+  "apiKey": "AIzaSyAmhQB4yUoskfJIoBme4OStNkpGzXUxR7c",
+  "authDomain": "greecole.firebaseapp.com",
+  "messagingSenderId": "97019754371"
 };
