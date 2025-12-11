23f2dd60fcbe56ae55ef5bfa593440e87eed58cc
Your Name <magloire078@gmail.com>
2025-12-11 14:18:11 +0000

    Try fixing this error: `Console Error: [2025-12-11T14:17:10.956Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import {initializeApp, getApp, getApps, FirebaseApp} from 'firebase/app';
 import {getAuth, Auth} from 'firebase/auth';
-import {getFirestore, Firestore} from 'firebase/firestore';
+import {getFirestore, Firestore, initializeFirestore, memoryLocalCache} from 'firebase/firestore';
 import { getStorage, FirebaseStorage } from "firebase/storage";
 import {firebaseConfig} from './config';
 export * from './provider';
@@ -21,11 +21,15 @@ function initializeFirebase() {
     if (!getApps().length) {
       app = initializeApp(firebaseConfig);
       auth = getAuth(app);
-      firestore = getFirestore(app);
+      // Use initializeFirestore for more granular control over settings
+      firestore = initializeFirestore(app, {
+        cache: memoryLocalCache(),
+      });
       storage = getStorage(app);
     } else {
       app = getApp();
       auth = getAuth(app);
+      // Ensure Firestore is also re-initialized or retrieved correctly
       firestore = getFirestore(app);
       storage = getStorage(app);
     }
