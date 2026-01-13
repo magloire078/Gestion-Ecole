79a8c403057c9748fe16c34ea3671aed6a517419
Magloire Kouadio <magloire078@gmail.com>
2026-01-13 10:35:09 +0000

    ok

--- src/app/dashboard/login/page.tsx
+++
@@ -1,8 +0,0 @@
-
-'use client';
-import { redirect } from 'next/navigation';
-
-// Redirige vers la nouvelle page de connexion
-export default function DeprecatedLoginPage() {
-    redirect('/auth/login');
-}
