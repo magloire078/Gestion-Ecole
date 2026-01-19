ce071e035c107b1d488becdc361a45155f78e1c2
Magloire Kouadio <magloire078@gmail.com>
2026-01-19 13:04:04 +0000

    le Storage pour les images, les logos des écoles, les photos des ecoles

--- next.config.js
+++ next.config.js
@@ -5,23 +5,27 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'firebasestorage.googleapis.com',
-        pathname: '**',
+        pathname: '/v0/b/greecole.firebasestorage.app/**',
       },
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
       },
       {
         protocol: 'https',
         hostname: 'picsum.photos',
+        pathname: '**',
       },
        {
         protocol: 'https',
         hostname: 'upload.wikimedia.org',
+        pathname: '**',
       },
       {
         protocol: 'https',
         hostname: 'www.pigroup360.com',
+        pathname: '**',
       },
     ],
   },
